7f77dc2c8617b19cd31351816836c3186c7585e4
Manvi Gupta <manvi.gupta@icidigital.com>
2018-09-16 07:43:37 -0400

    ember:url-and-template-different-in-route

--- udacityMealsApp/app/router.js
+++ udacityMealsApp/app/router.js
@@ -6,8 +6,13 @@ const Router = EmberRouter.extend({
   rootURL: config.rootURL
 });
 
+//added by the ember cli command --> ember g route menu. So, this menu route loads the menu routes file
+//present in the routes folder and then loads the menu template file.
 Router.map(function() {
   this.route('menu');
+  //if you want to have different route for the menu, for example : legos, you define like this.
+  //this.route('menu', { path: '/legos' });
+  // this will use the url as /legos and template as menu.hbs
 });
 
 export default Router;
