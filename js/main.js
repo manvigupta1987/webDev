c99444587db0bf2e09251422cec2a7d4fa0d17a4
Manvi Gupta <manvi.gupta@icidigital.com>
2018-09-20 14:41:03 -0400

    added alt tags

--- js/main.js
+++ js/main.js
@@ -161,6 +161,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.setAttribute("alt",restaurant.name);
   li.append(image);
 
   const divTag = document.createElement('div');
