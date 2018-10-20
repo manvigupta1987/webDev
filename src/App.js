29efe3e2190bcb78d8e224e3b3f1beea9b535c40
Manvi Gupta <manvi.gupta@icidigital.com>
2018-10-20 16:12:26 -0400

    remove gitignore files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from './Components/Map'
+import { GoogleApiWrapper } from 'google-maps-react'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <h1 className="app-name">Welcome to neighborhood map</h1>
         </header>
+        {
+          this.props.loaded? (
+            <Map google={this.props.google} />
+          ) : (<div class="map-error"> Could'nt load google maps </div>)
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyAYb1WQLh2JaKpVrdZegH69tVAI2LH9gNs'
+})(App)
---
+++ src/Components/Map.js
@@ -0,0 +1,164 @@
+import React, { Component } from 'react'
+
+class Map extends Component {
+
+	state = {
+		map: '',
+		infoWindow : {},
+		locations: [{
+          'title': "Carmen's Cuban Cafe & Lounge",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.86226399999999,
+            'lng': -78.81819100000001
+          },
+          'placeId': 'ChIJ58wYqsnxrIkR35nmdWvlVzY'
+        },
+        {
+          'title': "Babymoon Cafe",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.8512457,
+            'lng': -78.8270928
+          },
+          'placeId': 'ChIJM47UNN3xrIkRQoSk4tao5x4'
+        },
+        {
+          'title': "Bad Daddy's Burger Bar",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.80801,
+            'lng': -78.81897750000002
+          },
+          'placeId': 'ChIJ6YeE-j_yrIkRlV7JL6CbplM'
+        },
+        {
+          'title': "Mi Cancun",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.8200653,
+            'lng': -78.84567930000003
+          },
+          'placeId': 'ChIJlbm9afrtrIkRBf8o160LD-s'
+        },
+        {
+          'title': "La Farm",
+          'type': "bakery",
+          'position': {
+            'lat': 35.7906696,
+            'lng': -78.8274806
+          },
+          'placeId': 'ChIJXVEwV3vyrIkRgJmnZlguzq0'
+        },
+        {
+          'title': "Smokey's BBQ Shack",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.8497941,
+            'lng': -78.8391595
+          },
+          'placeId': 'ChIJl22V7x_urIkRAVHDxziyALE'
+        },
+        {
+          'title': "Lugano Ristorante",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.79994500000001,
+            'lng': -78.81612410000002
+          },
+          'placeId': 'ChIJn_9e-GnyrIkRkVpEDRJO320'
+        },
+        {
+          'title': "Texas Steakhouse & Saloon",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.8576266,
+            'lng': -78.82137210000002
+          },
+          'placeId': 'ChIJBegz1sXxrIkRtnXyJrfKTrY'
+        },
+        {
+          'title': "Doherty's Irish Pub & Restaurant",
+          'type': "restaurant",
+          'position': {
+            'lat': 35.7885254,
+            'lng': -78.84812620000002
+          },
+          'placeId': 'ChIJp-wS2ZztrIkRP1Xa2q30Ppo'
+        }]
+	}
+	componentDidMount () {
+		this.initMap()
+	}
+
+	setMarkers = (map) =>{
+		const mapLocations = []
+		this.state.locations.map((location) =>{
+			const marker = new window.google.maps.Marker({
+				position: new window.google.maps.LatLng(location.position),
+				animation: window.google.maps.Animation.DROP,
+				map,
+				title:location.title
+			})
+			location.marker = marker
+			location.visible = true
+			mapLocations.push(location)
+			marker.addListener('click', () => {
+				this.openInfoWindow(marker)
+			})
+		})
+
+		this.setState({locations: mapLocations})
+	}
+
+	toggleBounce = (marker)=> {
+		if (marker.getAnimation() !== null) {
+          marker.setAnimation(null);
+        } else {
+          marker.setAnimation(window.google.maps.Animation.BOUNCE);
+        }
+    }
+
+	openInfoWindow = (marker)=>{
+		this.state.infoWindow.open(this.state.map, marker)
+		this.state.infoWindow.setContent(marker.title)
+	}
+
+	closeInfoWindow = (marker) => {
+   	 this.state.infoWindow.close()
+  	}
+
+	initMap() {
+		if (this.props && this.props.google) {
+      		// google is available
+      		const map = new window.google.maps.Map(document.querySelector('#map'), {
+        		center: {
+          			lat: 35.823483,
+          			lng: -78.82556210000001
+        		},
+        		mapTypeControl: false,
+        		zoom: 13
+      		})
+      		const infoWindow = new window.google.maps.InfoWindow({})
+      		this.setState({map, infoWindow})
+      		this.setMarkers(map)
+
+      		window.google.maps.event.addListener(map, 'click', () => {
+        		this.closeInfoWindow()
+      		})
+      	}
+	}
+
+	render() {
+		const style = {
+      		width: '100%',
+      		height: '100%'
+    	}
+		return(
+			<div id='map' style={style}>
+			</div>
+		)
+	}
+}
+
+export default Map
