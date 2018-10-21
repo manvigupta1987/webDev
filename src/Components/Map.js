363cb6839b1a0d3c0b6f6cbaa70bcaf6c60a78e9
Manvi Gupta <manvi.gupta@icidigital.com>
2018-10-21 17:47:34 -0400

    added comments

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -6,10 +6,11 @@ import markerImg from '../images/dinner.png'
 
 class Map extends Component {
 
-	state = {
-		map: '',
-		infoWindow : {},
-		locations: [{
+  state = {
+    map: '',
+    infoWindow : {},
+    //locations
+    locations: [{
           'title': "Alpaca Peruvian Chicken",
           'type': "restaurant",
           'position': {
@@ -118,72 +119,82 @@ class Map extends Component {
           'placeId': '4b046caff964a520105422e3'
         }],
         oldMarker: ''
-	}
-	componentDidMount () {
-		this.initMap()
-	}
+  }
+  componentDidMount () {
+    this.initMap()
+  }
 
-	getMarkerDetails = (location)=> {
-		const client_secret = 'YJQZ5FTKIA5UHUDU2BNACLRW14WZBDQLOO0KIWNSBUC2QN4V'
-		const client_id = 'CFSMRM4YK0LMFIZIOO1ETN50A1TXPJENSO3EUOIEBXK3E5ER'
-		const foursquareUrl = `https://api.foursquare.com/v2/venues/${location.placeId}?client_id=${client_id}&client_secret=${client_secret}&v=20181018`
+  /* function is used to get the marker details using the foursquare api.
+  *@param: location
+  */
+  getMarkerDetails = (location)=> {
+    const client_secret = 'YJQZ5FTKIA5UHUDU2BNACLRW14WZBDQLOO0KIWNSBUC2QN4V'
+    const client_id = 'CFSMRM4YK0LMFIZIOO1ETN50A1TXPJENSO3EUOIEBXK3E5ER'
+    const foursquareUrl = `https://api.foursquare.com/v2/venues/${location.placeId}?client_id=${client_id}&client_secret=${client_secret}&v=20181018`
 
-		fetch(foursquareUrl)
-			.then((response) =>{
-				if(response.status !== 200){
-					this.state.infoWindow.setContent("No result found")
-					return
-				}
+    //fetch foursquare url for each place id to show the details.
+    fetch(foursquareUrl)
+      .then((response) =>{
+        if(response.status !== 200){
+          this.state.infoWindow.setContent("No result found")
+          return
+        }
 
-				response.json().then((data) => {
-        			const { venue } = data.response
-        			console.log(venue)
-        			this.state.infoWindow.setContent(`<div class='marker-info'>
-        				<h2 class="marker-name">${venue.name}</h2>
-        				<img class="marker-photo" src=${venue.bestPhoto ? `${venue.bestPhoto.prefix}300x200${venue.bestPhoto.suffix}` : "no phone found" } alt="restaurant photo">
-        				<h3 class="markerInfo">${venue.location.address? venue.location.address : venue.location.formattedAddress[0]}</h3>
-        				<p class="markerInfo"><strong>Contact Number: </em></strong>${venue.contact.formattedPhone? venue.contact.formattedPhone : "phone number not available"}</p>
-        				<p class="markerInfo"><strong><em>hours: </em></strong>${venue.hours ? venue.hours.status : "hours not available"}</p>
-        				<p class="markerInfo">${venue.rating ? '<strong><em>Rating: </em></strong>'+ venue.rating: ''}</p>
-        				<p class="markerInfo"><a href='${venue.canonicalUrl}' target='_blank'>Read more</a></p>
-        				<img class="foursquare-image"src=${foursquareImg} alt="Powered By foursquare"/>
-        				</div>`)
-      			})
-			})
-			.catch((err) => {
-				this.state.infoWindow.setContent("No result found")
-			})
+        response.json().then((data) => {
+              const { venue } = data.response
+              console.log(venue)
+              this.state.infoWindow.setContent(`<div class='marker-info'>
+                <h2 class="marker-name">${venue.name}</h2>
+                <img class="marker-photo" src=${venue.bestPhoto ? `${venue.bestPhoto.prefix}300x200${venue.bestPhoto.suffix}` : "no phone found" } alt="restaurant photo">
+                <h3 class="markerInfo">${venue.location.address? venue.location.address : venue.location.formattedAddress[0]}</h3>
+                <p class="markerInfo"><strong>Contact Number: </em></strong>${venue.contact.formattedPhone? venue.contact.formattedPhone : "phone number not available"}</p>
+                <p class="markerInfo"><strong><em>hours: </em></strong>${venue.hours ? venue.hours.status : "hours not available"}</p>
+                <p class="markerInfo">${venue.rating ? '<strong><em>Rating: </em></strong>'+ venue.rating: ''}</p>
+                <p class="markerInfo"><a href='${venue.canonicalUrl}' target='_blank'>Read more</a></p>
+                <img class="foursquare-image"src=${foursquareImg} alt="Powered By foursquare"/>
+                </div>`)
+            })
+      })
+      .catch((err) => {
+        this.state.infoWindow.setContent("No result found")
+      })
 
-	}
+  }
+  /* function to set the icon image of the marker.
+  *@param: marker: marker object
+  *@param: iconImg : image of the marker
+  */
+  setMarkerIcon = (marker, iconImg)=>{
+    marker.setIcon(iconImg)
+    this.state.map.panTo(marker.getPosition())
+  }
 
-	setMarkerIcon = (marker, iconImg)=>{
-		marker.setIcon(iconImg)
-		this.state.map.panTo(marker.getPosition())
-	}
+  setMarkers = (map) =>{
+    const mapLocations = this.state.locations.map((location) =>{
+      const marker = new window.google.maps.Marker({
+        position: new window.google.maps.LatLng(location.position),
+        animation: window.google.maps.Animation.DROP,
+        map,
+        title:location.title
+      })
+      marker.setIcon(markerImg);
+      location.longName = `${location.title}`
+      location.marker = marker
+      location.visible = true
+      marker.addListener('click', () => {
+        this.openInfoWindow(marker)
+        this.getMarkerDetails(location)
+      })
+      return location
+    })
+    this.setState({locations: mapLocations})
+  }
 
-	setMarkers = (map) =>{
-		const mapLocations = this.state.locations.map((location) =>{
-			const marker = new window.google.maps.Marker({
-				position: new window.google.maps.LatLng(location.position),
-				animation: window.google.maps.Animation.DROP,
-				map,
-				title:location.title
-			})
-			marker.setIcon(markerImg);
-			location.longName = `${location.title}`
-			location.marker = marker
-			location.visible = true
-			marker.addListener('click', () => {
-				this.openInfoWindow(marker)
-				this.getMarkerDetails(location)
-			})
-			return location
-		})
-		this.setState({locations: mapLocations})
-	}
-
-	toggleBounce = (marker)=> {
-		if (marker.getAnimation() !== null) {
+  /* function to set or unset the animation when marker is clicked
+  *@param: marker object
+  */
+  toggleBounce = (marker)=> {
+    if (marker.getAnimation() !== null) {
           marker.setAnimation(null);
         } else {
           marker.setAnimation(window.google.maps.Animation.BOUNCE);
@@ -191,64 +202,71 @@ class Map extends Component {
         }
     }
 
-	openInfoWindow = (marker)=>{
-		this.closeInfoWindow()
-		this.state.infoWindow.open(this.state.map, marker)
-		this.state.infoWindow.setContent(marker.title)
-		this.toggleBounce(marker)
-		marker.setIcon(selectedMarker)
-		this.setState({oldMarker: marker})
-		this.state.map.panTo(marker.getPosition())
-	}
+  /* function to open the marker details window when marker is clicked
+  *@param: marker
+  */
+  openInfoWindow = (marker)=>{
+    this.closeInfoWindow()
+    this.state.infoWindow.open(this.state.map, marker)
+    this.state.infoWindow.setContent(marker.title)
+    this.toggleBounce(marker)
+    marker.setIcon(selectedMarker)
+    this.setState({oldMarker: marker})
+    this.state.map.panTo(marker.getPosition())
+  }
 
-	closeInfoWindow = () => {
-		if(this.state.oldMarker){
-			this.state.oldMarker.setIcon(markerImg)
-			this.toggleBounce(this.state.oldMarker)
-		}
-		this.setState({oldMarker: ''})
-   		this.state.infoWindow.close()
-  	}
+  /* function to close the marker details window when marker is clicked
+  *@param: marker
+  */
+  closeInfoWindow = () => {
+    if(this.state.oldMarker){
+      this.state.oldMarker.setIcon(markerImg)
+      this.toggleBounce(this.state.oldMarker)
+    }
+    this.setState({oldMarker: ''})
+      this.state.infoWindow.close()
+    }
 
-	initMap() {
-		if (this.props && this.props.google) {
-      		// google is available
-      		const map = new window.google.maps.Map(document.querySelector('#map'), {
-        		center: {
-          			lat: 35.823483,
-          			lng: -78.82556210000001
-        		},
-        		mapTypeControl: false,
-        		zoom: 13
-      		})
-      		const infoWindow = new window.google.maps.InfoWindow({})
-      		this.setState({map, infoWindow})
-      		this.setMarkers(map)
+  /* function to load the map and infowindow*/
+  initMap() {
+    if (this.props && this.props.google) {
+      // google is available
+      const map = new window.google.maps.Map(document.querySelector('#map'), {
+        center: {
+          lat: 35.823483,
+          lng: -78.82556210000001
+        },
+        mapTypeControl: false,
+        zoom: 13
+      })
+      const infoWindow = new window.google.maps.InfoWindow({})
+      this.setState({map, infoWindow})
+      this.setMarkers(map)
 
-      		window.google.maps.event.addListener(map, 'click', () => {
-        		this.closeInfoWindow()
-      		})
-      	}
-	}
+      window.google.maps.event.addListener(map, 'click', () => {
+        this.closeInfoWindow()
+      })
+    }
+  }
 
-	render() {
-		const style = {
-      		width: '100%',
-      		height: '100%'
-    	}
-		return(
-			<div>
-				<PlacesList
-					locations={this.state.locations}
-					openInfoWindow = {this.openInfoWindow}
-					closeInfoWindow = {this.closeInfoWindow}
-					getMarkerInfo = {this.getMarkerDetails}
-					setMarkerIcon = {this.setMarkerIcon}
-				/>
-				<div id='map' role='application' style={style}></div>
-			</div>
-		)
-	}
+  render() {
+    const style = {
+          width: '100%',
+          height: '100%'
+      }
+    return(
+      <div>
+        <PlacesList
+          locations={this.state.locations}
+          openInfoWindow = {this.openInfoWindow}
+          closeInfoWindow = {this.closeInfoWindow}
+          getMarkerInfo = {this.getMarkerDetails}
+          setMarkerIcon = {this.setMarkerIcon}
+        />
+        <div id='map' role='application' style={style}></div>
+      </div>
+    )
+  }
 }
 
 export default Map
